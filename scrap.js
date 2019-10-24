4e6f767ab2e0d0411392e1051843f06b82a2a758
Pejantantangguh <suherman.ng@gmail.com>
2019-10-24 10:44:35 +1000

    Scrapping work on Leap Australia

--- scrap.js
+++ scrap.js
@@ -1,13 +1,13 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://www.leapaust.com.au/product-category/3dconnexion/', (err,res,html) => {
-    if(!err && res.statusCode == 200 ) {
+request('https://www.leapaust.com.au/3dconnexion/', (err, res, html) => {
+    if (!err && res.statusCode == 200) {
         const $ = cheerio.load(html);
         const productName = $('h6.mpcth-post-title');
         const productPrice = $('span.woocommerce-Price-amount.amount');
-        
-        
+
+
         console.log(productName.children('a').text());
         console.log(productPrice.text());
     }
---
+++ scrapTocsv.js
@@ -0,0 +1,44 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('leapaustralia.csv');
+
+// Write headers
+
+writeStream.write(`Product title,price,link \n`)
+
+request('https://www.leapaust.com.au/3dconnexion/', (err, res, html) => {
+    if (!err && res.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        $('.product.type-product').each((i, element) => {
+            const title = $(element)
+                .find('.mpcth-post-title')
+                .text();
+            const price = $(element)
+                .find('.mpcth-cart-wrap .price .woocommerce-Price-amount.amount')
+                .text()
+                .parseFloat();
+
+            const link = $(element)
+                .find('.mpcth-post-title > a')
+                .attr('href');
+
+            // Write row to CSV
+
+            writeStream.write(`${title}, ${price}, ${link} \n`);
+        });
+
+        console.log('Scrap Done...');
+    }
+});
+
+/***
+ * My case
+    Every product lives in article class of product type-product == product.type-product
+    Product information lives in div.mpcth-product-wrap > section.mpcth-post-content
+    Inside mpcth-post-content lives product title and pricing
+
+
+
+ * */
